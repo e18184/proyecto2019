01f6bcad85e045704320959fed40e66ad42f06e1
Rodrigo Alvarez <alumno@alumno-virtual-machine>
2019-11-29 07:56:51 -0800

    controlador docente

---
+++ src/java/controlador/docente/RealizarAnuncioControlador.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador.docente;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ *
+ * @author alumno
+ */
+public class RealizarAnuncioControlador {
+
+    @RequestMapping("realizaranuncio.htm")
+    public ModelAndView realizaranuncio() {
+        return null;
+    }
+}
---
+++ web/WEB-INF/jsp/cargarnotas.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : cargarnotas
+    Created on : Nov 29, 2019, 7:43:31 AM
+    Author     : alumno
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Cargar Notas</title>
+    </head>
+    <body>
+        <h1>Cargar Notas</h1>
+    </body>
+</html>
